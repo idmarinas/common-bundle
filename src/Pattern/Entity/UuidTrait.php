5dff7f1ea33a77e05f4338864a9c28bf6be30c5c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-11-05 16:54:15 +0100

    First commit

---
+++ src/Pattern/Entity/UuidTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of Bundle "IdmCommonBundle".
+ *
+ * @see https://github.com/idmarinas/common-bundle/
+ *
+ * @license https://github.com/idmarinas/common-bundle/blob/master/LICENSE.txt
+ *
+ * @since 1.1.0
+ */
+
+namespace Idm\Bundle\Common\Pattern\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+use Ramsey\Uuid\UuidInterface;
+
+trait UuidTrait
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
+     */
+    private ?UuidInterface $uuid = null;
+
+    public function getUuid(): ?UuidInterface
+    {
+        return $this->uuid;
+    }
+}
