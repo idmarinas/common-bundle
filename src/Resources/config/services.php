b48b1af1c3551cd8f12db7212708f9eb866eb839
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-12-31 16:14:53 +0100

    Add command service

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -12,8 +12,12 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Idm\Bundle\Common\Command\RegenerateAppSecretCommand;
+
 return static function (ContainerConfigurator $container)
 {
     $container->services()
+        ->set('idm.common.command.reg_app_secret', RegenerateAppSecretCommand::class)
+            ->tag('console.command')
     ;
 };
