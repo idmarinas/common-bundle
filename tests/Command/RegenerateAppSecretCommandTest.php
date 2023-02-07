b8d1095aadd3197e4ee1d617f60e61fbdfbe307c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2023-02-07 16:33:17 +0100

    Add new test

--- tests/Command/RegenerateAppSecretCommandTest.php
+++ tests/Command/RegenerateAppSecretCommandTest.php
@@ -15,9 +15,13 @@ namespace App\Tests\Command;
 use Idm\Bundle\Common\Command\RegenerateAppSecretCommand;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Filesystem\Filesystem;
 
 class RegenerateAppSecretCommandTest extends KernelTestCase
 {
+    private string $cFile = '.env';
+    private string $cKey  = 'APP_SECRET';
+
     public function testExecuteShow()
     {
         $commandTester = new CommandTester(new RegenerateAppSecretCommand());
@@ -38,5 +42,54 @@ class RegenerateAppSecretCommandTest extends KernelTestCase
         $this->assertStringContainsString('New APP_SECRET', $output);
 
         // ...
+    }
+
+    public function testNotFile()
+    {
+        $commandTester = new CommandTester(new RegenerateAppSecretCommand());
+        $commandTester->execute([
+            '--show' => false,
+        ]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Not find file ".env" or not is readable or writable.', $output);
+    }
+
+    public function testNotSecret()
+    {
+        $fs            = new Filesystem();
+        $commandTester = new CommandTester(new RegenerateAppSecretCommand());
+
+        $fs->touch($this->cFile);
+
+        $commandTester->execute([
+            '--show' => false,
+        ]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString("Not find {$this->cKey} in file '{$this->cFile}", $output);
+
+        $fs->remove($this->cFile);
+    }
+
+    public function testSecret()
+    {
+        $fs            = new Filesystem();
+        $commandTester = new CommandTester(new RegenerateAppSecretCommand());
+
+        $fs->touch($this->cFile);
+        $fs->dumpFile($this->cFile, 'APP_SECRET=80196600742574c00dc7d7c02224284c');
+
+        $commandTester->execute([
+            '--show' => false,
+        ]);
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString("New {$this->cKey} was generated: ", $output);
+
+        $fs->remove($this->cFile);
     }
 }
