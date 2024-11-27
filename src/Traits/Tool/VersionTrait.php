f718a11192de70457ff44e3ecb0025d7088938b5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-27 18:00:58 +0100

    Formated

--- src/Traits/Tool/VersionTrait.php
+++ src/Traits/Tool/VersionTrait.php
@@ -1,47 +1,50 @@
 <?php
-
 /**
- * This file is part of Bundle "IdmCommonBundle".
+ * Copyright 2023-2024 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/common-bundle/
+ * Last modified by "IDMarinas" on 27/11/24, 17:58
  *
- * @license https://github.com/idmarinas/common-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
  *
- * @since 1.3.0
+ * @file    VersionTrait.php
+ * @date    13/04/2023
+ * @time    16:35
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.3.0
  */
 
 namespace Idm\Bundle\Common\Traits\Tool;
 
-use vierbergenlars\SemVer\version;
-
 trait VersionTrait
 {
     /** Conver int version like 100000000 to 1.0.0 */
-    public function convertVersionToString(int $version): string
+    public function convertVersionToString (int $version): string
     {
-        $version = (string) $version;
+        $version = (string)$version;
 
-        $path    = (int) substr($version, -4);
+        $path = (int)substr($version, -4);
         $version = substr_replace($version, '', -4);
-        $minor   = (int) substr($version, -4);
+        $minor = (int)substr($version, -4);
         $version = substr_replace($version, '', -4);
-        $major   = (int) substr($version, -4);
+        $major = (int)substr($version, -4);
 
         return "{$major}.{$minor}.{$path}";
     }
 
     /** Convert string version like 1.0.0 to 100000000 */
-    public function convertVersionToInt(string $version)
+    public function convertVersionToInt (string $version)
     {
         $re = '/^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/m';
 
         preg_match_all($re, $version, $matches, PREG_SET_ORDER, 0);
         $matches = $matches[0];
 
-        return (int) (
-            str_pad($matches['major'], 4, 0, STR_PAD_LEFT)
-            .str_pad($matches['minor'], 4, 0, STR_PAD_LEFT)
-            .str_pad($matches['patch'], 4, 0, STR_PAD_LEFT)
-        );
+        return (int)(str_pad($matches['major'], 4, 0, STR_PAD_LEFT) . str_pad(
+                $matches['minor'], 4, 0, STR_PAD_LEFT
+            ) . str_pad($matches['patch'], 4, 0, STR_PAD_LEFT));
     }
 }
