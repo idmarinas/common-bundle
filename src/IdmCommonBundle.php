bb07c5c31c0921b8b1867dadd196710e45239560
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-27 14:28:30 +0100

    Updated copyright

--- src/IdmCommonBundle.php
+++ src/IdmCommonBundle.php
@@ -1,19 +1,24 @@
 <?php
-
 /**
- * This file is part of Bundle "IdmCommonBundle".
+ * Copyright 2022-2024 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/common-bundle/
+ * Last modified by "IDMarinas" on 27/11/24, 14:28
  *
- * @license https://github.com/idmarinas/common-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
  *
- * @since 1.0.0
+ * @file    IdmCommonBundle.php
+ * @date    14/08/2022
+ * @time    18:01
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
  */
 
 namespace Idm\Bundle\Common;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class IdmCommonBundle extends Bundle
-{
-}
+class IdmCommonBundle extends Bundle {}
