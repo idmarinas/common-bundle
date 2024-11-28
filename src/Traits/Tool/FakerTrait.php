4eaa5bbe9aa3ba94fa60ca1e28b6687fab77dfe6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-28 13:38:53 +0100

    Updated copyright

--- src/Traits/Tool/FakerTrait.php
+++ src/Traits/Tool/FakerTrait.php
@@ -1,13 +1,20 @@
 <?php
-
 /**
- * This file is part of Bundle "IdmCommonBundle".
+ * Copyright 2023-2024 (C) IDMarinas - All Rights Reserved
  *
- * @see https://github.com/idmarinas/common-bundle/
+ * Last modified by "IDMarinas" on 28/11/24, 13:38
  *
- * @license https://github.com/idmarinas/common-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
  *
- * @since 1.5.0
+ * @file    FakerTrait.php
+ * @date    13/04/2023
+ * @time    16:35
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.5.0
  */
 
 namespace Idm\Bundle\Common\Traits\Tool;
@@ -20,15 +27,13 @@ trait FakerTrait
 {
     private ?Generator $faker = null;
 
-    public function faker(): Generator
+    public function faker (): Generator
     {
-        if ( ! class_exists(Factory::class))
-        {
+        if (!class_exists(Factory::class)) {
             throw new LogicException('Faker PHP missing. Try running "composer require --dev fakerphp/faker".');
         }
 
-        if ( ! $this->faker instanceof Generator)
-        {
+        if (!$this->faker instanceof Generator) {
             $this->faker = Factory::create();
         }
 
