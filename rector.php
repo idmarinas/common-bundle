3e92493ce706b5d1bcb40dff082800ca24f11c4a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-02 16:08:01 +0100

    Formateado

--- rector.php
+++ rector.php
@@ -7,20 +7,25 @@ use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
-                   ->withPaths([
-                       __DIR__ . '/src',
-                       __DIR__ . '/tests',
-                   ])
-                   ->withPhpSets(php80: true)
-                   ->withPreparedSets(
-                       deadCode          : true, codeQuality: true, codingStyle: true, doctrineCodeQuality: true,
-                       symfonyCodeQuality: true, symfonyConfigs: true, twig: true
-                   )
-                   ->withImportNames(removeUnusedImports: true)
-                   ->withTypeCoverageLevel(0)
-                   ->withSets([
-                       SymfonySetList::SYMFONY_54,
-                       SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-                   ])
-                   ->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php80: true)
+    ->withPreparedSets(
+        deadCode           : true,
+        codeQuality        : true,
+        codingStyle        : true,
+        doctrineCodeQuality: true,
+        symfonyCodeQuality : true,
+        symfonyConfigs     : true,
+        twig               : true
+    )
+    ->withImportNames(removeUnusedImports: true)
+    ->withTypeCoverageLevel(0)
+    ->withSets([
+        SymfonySetList::SYMFONY_54,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ])
+    ->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
 ;
