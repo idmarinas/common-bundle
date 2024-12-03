4b51e92ac9d816cccc21c51acbe49bb38735c7f0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-03 21:37:45 +0100

    Updated min PHP Version to 8.1

--- rector.php
+++ rector.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright 2024 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 03/12/2024, 21:37
+ *
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    rector.php
+ * @date    03/12/2024
+ * @time    21:37
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   3.2.0
+ */
 
 declare(strict_types=1);
 
@@ -7,25 +24,25 @@ use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
-    ->withPhpSets(php80: true)
-    ->withPreparedSets(
-        deadCode           : true,
-        codeQuality        : true,
-        codingStyle        : true,
-        doctrineCodeQuality: true,
-        symfonyCodeQuality : true,
-        symfonyConfigs     : true,
-        twig               : true
-    )
-    ->withImportNames(removeUnusedImports: true)
-    ->withTypeCoverageLevel(0)
-    ->withSets([
-        SymfonySetList::SYMFONY_54,
-        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-    ])
-    ->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
+	->withPaths([
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php81: true)
+	->withPreparedSets(
+		deadCode           : true,
+		codeQuality        : true,
+		codingStyle        : true,
+		doctrineCodeQuality: true,
+		symfonyCodeQuality : true,
+		symfonyConfigs     : true,
+		twig               : true
+	)
+	->withImportNames(removeUnusedImports: true)
+	->withTypeCoverageLevel(0)
+	->withSets([
+		SymfonySetList::SYMFONY_54,
+		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+	])
+	->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
 ;
