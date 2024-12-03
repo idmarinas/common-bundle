d39de3d88b91b14ff55a45bb17ba509ce55b8fc9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-12-03 20:52:06 +0100

    Added new method for populate an entity

--- src/Traits/Tool/FakerTrait.php
+++ src/Traits/Tool/FakerTrait.php
@@ -19,24 +19,81 @@
 
 namespace Idm\Bundle\Common\Traits\Tool;
 
+use Doctrine\ORM\Mapping\Column;
 use Faker\Factory;
 use Faker\Generator;
 use LogicException;
+use ReflectionClass;
+use ReflectionProperty;
+use Symfony\Component\Uid\Uuid;
 
 trait FakerTrait
 {
-    private ?Generator $faker = null;
+	private ?Generator $faker = null;
 
-    public function faker (): Generator
-    {
-        if (!class_exists(Factory::class)) {
-            throw new LogicException('Faker PHP missing. Try running "composer require --dev fakerphp/faker".');
-        }
+	public function faker (): Generator
+	{
+		if (!class_exists(Factory::class)) {
+			throw new LogicException('Faker PHP missing. Try running "composer require --dev fakerphp/faker".');
+		}
 
-        if (!$this->faker instanceof Generator) {
-            $this->faker = Factory::create();
-        }
+		if (!$this->faker instanceof Generator) {
+			$this->faker = Factory::create();
+		}
 
-        return $this->faker;
-    }
+		return $this->faker;
+	}
+
+	public function populateEntity (object $entity): object
+	{
+		$reflectionClass = new ReflectionClass($entity);
+		$properties = $reflectionClass->getProperties();
+
+		foreach ($properties as $property) {
+			$property->setAccessible(true);
+			$property->setValue($entity, $this->fakerValueFromProperty($property));
+		}
+
+		return $entity;
+	}
+
+	private function fakerValueFromProperty (ReflectionProperty $property): mixed
+	{
+		return match ($property->getName()) {
+			'email'       => $this->faker()->email(),
+			'name',
+			'displayName' => $this->faker()->name(),
+			'lastName'    => $this->faker()->lastName(),
+			'password'    => $this->faker()->password(),
+			'username'    => $this->faker()->userName(),
+			'sessionId'   => $this->faker()->sha1(),
+			default       => $this->fakerValueForType($property),
+		};
+	}
+
+	private function fakerValueForType (ReflectionProperty $property): mixed
+	{
+		$attribute = $property->getAttributes(Column::class)[0];
+		$arguments = $attribute->getArguments();
+
+		return match ($arguments['type']) {
+			'text'                 => $this->faker()->text(),
+			'integer'              => $this->faker()->randomNumber(327539810),
+			'smallint'             => $this->faker()->randomNumber(32760),
+			'datetime',
+			'datetime_immutable',
+			'datetimetz',
+			'datetimetz_immutable' => $this->faker()->dateTime(),
+			'json', 'array'        => $this->faker()->words(),
+			'simple_array'         => implode(',', $this->faker()->words()),
+			'decimal'              => $this->faker()->randomFloat(2),
+			'uuid'                 => new Uuid($this->faker()->uuid()),
+			'date',
+			'date_inmutable'       => $this->faker()->date(),
+			'time',
+			'time_immutable'       => $this->faker()->time(),
+			'string'               => $this->faker()->text($arguments['length'] * 0.9),
+			default                => $this->faker()->sentence()
+		};
+	}
 }
