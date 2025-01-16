5be641619b802d7db19b6d5646d6579c1305d532
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-16 21:36:30 +0100

    update(tests): refactor ContainsIsEvenValidatorTest and ContainsIsOddValidatorTest

--- tests/Validator/ContainsIsOddValidatorTest.php
+++ tests/Validator/ContainsIsOddValidatorTest.php
@@ -1,11 +1,19 @@
 <?php
 
 /**
- * This file is part of Bundle "IdmCommonBundle".
+ * Copyright 2022-2025 (C) IDMarinas - All Rights Reserved
  *
- * @see     https://github.com/idmarinas/common-bundle/
+ * Last modified by "IDMarinas" on 16/01/2025, 21:36
  *
- * @license https://github.com/idmarinas/common-bundle/blob/master/LICENSE.txt
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    ContainsIsOddValidatorTest.php
+ * @date    05/11/2022
+ * @time    18:32
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
  *
  * @since   1.1.0
  */
@@ -21,60 +29,60 @@ use Throwable;
 
 class ContainsIsOddValidatorTest extends ConstraintValidatorTestCase
 {
-    public function testNullIsValid (): void
-    {
-        $this->validator->validate(null, new IsOdd());
+	public function testNullIsValid (): void
+	{
+		$this->validator->validate(null, new IsOdd());
 
-        $this->assertNoViolation();
-    }
+		$this->assertNoViolation();
+	}
 
-    public function testEmptyIsValid (): void
-    {
-        $this->validator->validate('', new IsOdd());
+	public function testEmptyIsValid (): void
+	{
+		$this->validator->validate('', new IsOdd());
 
-        $this->assertNoViolation();
-    }
+		$this->assertNoViolation();
+	}
 
-    public function testConstraintInvalid (): void
-    {
-        try {
-            $this->validator->validate(5, new IsEven());
-            $this->fail('Fail expect a exception for argument of Constraint not is correct');
-        } catch (Throwable) {
-            // -- Expected argument of type "Idm\Bundle\Common\Validator\Constraints\IsEven", "Idm\Bundle\Common\Validator\Constraints\IsOdd" given
-            $this->assertNoViolation();
-        }
-    }
+	public function testConstraintInvalid (): void
+	{
+		try {
+			$this->validator->validate(5, new IsEven());
+			$this->fail('Fail expect a exception for argument of Constraint not is correct');
+		} catch (Throwable) {
+			// -- Expected argument of type "Idm\Bundle\Common\Validator\Constraints\IsEven", "Idm\Bundle\Common\Validator\Constraints\IsOdd" given
+			$this->assertNoViolation();
+		}
+	}
 
-    public function testNotValidNumber (): void
-    {
-        try {
-            $this->validator->validate('rr', new IsOdd());
-            $this->fail('Fail expect a exception for argument of type "int|float"');
-        } catch (Throwable) {
-            // -- Expected argument of type "int|float", "string" given
-            $this->assertNoViolation();
-        }
-    }
+	public function testNotValidNumber (): void
+	{
+		try {
+			$this->validator->validate('rr', new IsOdd());
+			$this->fail('Fail expect a exception for argument of type "int|float"');
+		} catch (Throwable) {
+			// -- Expected argument of type "int|float", "string" given
+			$this->assertNoViolation();
+		}
+	}
 
-    public function testIsInvalid (): void
-    {
-        $this->validator->validate(6, new IsOdd());
+	public function testIsInvalid (): void
+	{
+		$this->validator->validate(6, new IsOdd());
 
-        $this
-            ->buildViolation('idm.common.is_odd')->setParameter('{{ number }}', 6)->assertRaised()
-        ;
-    }
+		$this
+			->buildViolation('idm.common.is_odd')->setParameter('{{ number }}', 6)->assertRaised()
+		;
+	}
 
-    public function testIsValid (): void
-    {
-        $this->validator->validate(5, new IsOdd());
+	public function testIsValid (): void
+	{
+		$this->validator->validate(5, new IsOdd());
 
-        $this->assertNoViolation();
-    }
+		$this->assertNoViolation();
+	}
 
-    protected function createValidator (): ConstraintValidatorInterface
-    {
-        return new IsOddValidator();
-    }
+	protected function createValidator (): ConstraintValidatorInterface
+	{
+		return new IsOddValidator();
+	}
 }
