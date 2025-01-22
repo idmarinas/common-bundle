9ee002e7fffd4df8114c702c8c429098ab3a5cda
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-22 13:27:58 +0100

    fix(doctrine): update database URL format to include dynamic DB name
    - Refactored the database URL in doctrine.php to use a dynamic name
      derived from the IdmCommonBundle class.
    - Added imports for ReflectionClass and Symfony String utility functions.

--- app/config/packages/doctrine.php
+++ app/config/packages/doctrine.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 16/01/2025, 21:21
+ * Last modified by "IDMarinas" on 22/01/2025, 13:25
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -19,7 +19,10 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Idm\Bundle\Common\IdmCommonBundle;
+use ReflectionClass;
 use Symfony\Component\Filesystem\Filesystem;
+use function Symfony\Component\String\u;
 
 return static function (ContainerConfigurator $container) {
 	$getDatabaseCache = function (): string {
@@ -34,10 +37,13 @@ return static function (ContainerConfigurator $container) {
 		return $dir;
 	};
 
+	$dbName = (new ReflectionClass(IdmCommonBundle::class))->getShortName();
+	$dbName = u($dbName)->snake()->toString();
+
 	$container->extension('doctrine', [
 		'dbal' => [
 			'driver'         => 'pdo_sqlite',
-			'url'            => sprintf('sqlite:///%s/idm_user_%s.sqlite', $getDatabaseCache(), $container->env()),
+			'url'            => sprintf('sqlite:///%s/%s_%s.sqlite', $getDatabaseCache(), $dbName, $container->env()),
 			'use_savepoints' => true,
 		],
 		'orm'  => [
