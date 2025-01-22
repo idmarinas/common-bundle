e73846b992794c26ebf9718b839bff136baa0a44
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-22 18:53:44 +0100

    feat(tests): add BundleRoutingTest for routing functionality validation

---
+++ tests/BundleRoutingTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 22/01/2025, 13:07
+ *
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    BundleRoutingTest.php
+ * @date    22/01/2025
+ * @time    13:07
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   3.4.0
+ */
+
+namespace Idm\Bundle\Common\Tests;
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class BundleRoutingTest extends KernelTestCase
+{
+	protected static function createKernel (array $options = []): KernelInterface
+	{
+		/**
+		 * @var Kernel $kernel
+		 */
+		$kernel = parent::createKernel($options);
+		$kernel->handleOptions($options);
+
+		return $kernel;
+	}
+
+	public function testAddRoutingFile (): void
+	{
+		$kernel = self::bootKernel();
+
+		$container = $kernel->getContainer();
+		$container = $container->get('test.service_container');
+		/**
+		 * @var RouterInterface $router
+		 */
+		$router = $container->get(RouterInterface::class);
+		$routeCollection = $router->getRouteCollection();
+		$routes = $routeCollection->all();
+
+		$this->assertCount(1, $routes);
+		$this->assertNotNull($routeCollection->get('app_home'));
+	}
+}
