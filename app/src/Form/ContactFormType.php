7645d36d98a7edc79c113f80b2b3ac3be34eb470
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-23 16:20:47 +0100

    feat(form): add ContactFormType class for handling contact forms

---
+++ app/src/Form/ContactFormType.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 22/01/2025, 19:37
+ *
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    ContactFormType.php
+ * @date    22/01/2025
+ * @time    19:36
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   3.4.0
+ */
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Idm\Bundle\Common\Model\Form\AbstractContactFormType;
+
+class ContactFormType extends AbstractContactFormType {}
