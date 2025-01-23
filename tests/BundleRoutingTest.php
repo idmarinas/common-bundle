1190c691a217d93d55688639722f1226cc02dfad
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-23 17:07:39 +0100

    test: update route count assertion in BundleRoutingTest
    - Changed the expected count of routes from 1 to 2 in the test case.
    - Added assertion to check for the existence of 'idm_common_contact' route.

--- tests/BundleRoutingTest.php
+++ tests/BundleRoutingTest.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 22/01/2025, 13:07
+ * Last modified by "IDMarinas" on 23/01/2025, 17:05
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -50,7 +50,8 @@ class BundleRoutingTest extends KernelTestCase
 		$routeCollection = $router->getRouteCollection();
 		$routes = $routeCollection->all();
 
-		$this->assertCount(1, $routes);
+		$this->assertCount(2, $routes);
 		$this->assertNotNull($routeCollection->get('app_home'));
+		$this->assertNotNull($routeCollection->get('idm_common_contact'));
 	}
 }
