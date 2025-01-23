2ce62c86a30b999983fc356d41a6efb0bcadd0eb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-01-23 21:19:57 +0100

    test: update formatting and assertions in VersionTraitTest
    - Adjusted spacing in PHPDoc comments for consistency.
    - Reformatted test methods for better readability.
    - Ensured assertion statements maintain consistent indentation.

--- tests/Traits/Tool/VersionTraitTest.php
+++ tests/Traits/Tool/VersionTraitTest.php
@@ -1,13 +1,30 @@
 <?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 23/01/2025, 21:19
+ *
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    VersionTraitTest.php
+ * @date    23/01/2025
+ * @time    21:19
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   3.4.0
+ */
 
 /**
  * This file is part of Bundle "IdmCommonBundle".
  *
- * @see https://github.com/idmarinas/common-bundle/
+ * @see     https://github.com/idmarinas/common-bundle/
  *
  * @license https://github.com/idmarinas/common-bundle/blob/master/LICENSE.txt
  *
- * @since 1.3.0
+ * @since   1.3.0
  */
 
 namespace Idm\Bundle\Common\Tests\Traits\Tool;
@@ -17,49 +34,48 @@ use PHPUnit\Framework\TestCase;
 
 class VersionTraitTest extends TestCase
 {
-    public function testValidIntVersion(): void
-    {
-        $check = new VersionCheck();
+	public function testValidIntVersion (): void
+	{
+		$check = new VersionCheck();
 
-        $this->assertEquals(1_0000_0000, $check->convertVersionToInt('1.0.0'));
+		$this->assertEquals(1_0000_0000, $check->convertVersionToInt('1.0.0'));
 
-        $this->assertEquals(2_0010_0005, $check->convertVersionToInt('2.10.5'));
+		$this->assertEquals(2_0010_0005, $check->convertVersionToInt('2.10.5'));
 
-        $this->assertEquals(5_0003_0010, $check->convertVersionToInt('5.3.10'));
+		$this->assertEquals(5_0003_0010, $check->convertVersionToInt('5.3.10'));
 
-        $this->assertEquals(10_0269_6987, $check->convertVersionToInt('10.269.6987'));
+		$this->assertEquals(10_0269_6987, $check->convertVersionToInt('10.269.6987'));
 
-        $this->assertEquals(189_0785, $check->convertVersionToInt('0.189.785'));
-    }
+		$this->assertEquals(189_0785, $check->convertVersionToInt('0.189.785'));
+	}
 
-    // public function testInvalidIntVersion(): void
-    // {
+	// public function testInvalidIntVersion(): void
+	// {
 
-    // }
+	// }
 
-    public function testValidStringVersion(): void
-    {
-        $check = new VersionCheck();
+	public function testValidStringVersion (): void
+	{
+		$check = new VersionCheck();
 
-        $this->assertEquals('1.0.0', $check->convertVersionToString(1_0000_0000));
+		$this->assertEquals('1.0.0', $check->convertVersionToString(1_0000_0000));
 
-        $this->assertEquals('2.10.5', $check->convertVersionToString(2_0010_0005));
+		$this->assertEquals('2.10.5', $check->convertVersionToString(2_0010_0005));
 
-        $this->assertEquals('5.3.10', $check->convertVersionToString(5_0003_0010));
+		$this->assertEquals('5.3.10', $check->convertVersionToString(5_0003_0010));
 
-        $this->assertEquals('10.269.6987', $check->convertVersionToString(10_0269_6987));
+		$this->assertEquals('10.269.6987', $check->convertVersionToString(10_0269_6987));
 
-        $this->assertEquals('0.189.785', $check->convertVersionToString(189_0785));
+		$this->assertEquals('0.189.785', $check->convertVersionToString(189_0785));
+	}
 
-    }
+	// public function testInvalidStringVersion(): void
+	// {
 
-    // public function testInvalidStringVersion(): void
-    // {
-
-    // }
+	// }
 }
 
 class VersionCheck
 {
-    use VersionTrait;
+	use VersionTrait;
 }
