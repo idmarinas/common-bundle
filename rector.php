cdcc568402d4edbbf921028f4ec5118bacd01814
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-08 13:03:13 +0100

    fix(rector): update formatting and comments in rector.php file

--- rector.php
+++ rector.php
@@ -2,26 +2,25 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 16/01/2025, 21:03
+ * Last modified by "IDMarinas" on 08/02/2025, 13:02
  *
  * @project IDMarinas Common Bundle
- * @see https://github.com/idmarinas/idm-template-bundle
+ * @see     https://github.com/idmarinas/common-bundle
  *
- * @file rector.php
- * @date 03/12/2024
- * @time 21:37
+ * @file    rector.php
+ * @date    03/12/2024
+ * @time    21:37
  *
- * @author Iván Diaz Marinas (IDMarinas)
+ * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since 3.2.0
+ * @since   3.2.0
  */
 
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
 	->withPaths([
@@ -47,5 +46,4 @@ return RectorConfig::configure()
 		SymfonySetList::SYMFONY_64,
 		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
 	])
-	->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
 ;
