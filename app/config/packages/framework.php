8f08d525546ee7019f7b8cdc584a5cd11256ddd7
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-24 18:39:02 +0100

    feat(framework): enable CSRF protection in framework configuration
    - Added 'csrf_protection' setting to the framework configuration to
      enhance security measures. This change ensures that CSRF protection
      is enabled for forms processed by the application.

--- app/config/packages/framework.php
+++ app/config/packages/framework.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 24/02/2025, 18:28
+ * Last modified by "IDMarinas" on 24/02/2025, 18:38
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -28,6 +28,7 @@ return static function (ContainerConfigurator $container) {
 		'php_errors'            => [
 			'log' => true,
 		],
+		'csrf_protection'       => true,
 		'form'                  => true,
 		'assets'                => true,
 	]);
