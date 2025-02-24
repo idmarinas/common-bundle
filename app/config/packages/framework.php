99d132294f5348c1843b9836db971d84d76daec3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-24 18:29:03 +0100

    fix(framework): enable form protection and remove csrf settings
    - Updated the framework configuration to enable form protection.
    - Removed unnecessary csrf protection settings for cleaner code.

--- app/config/packages/framework.php
+++ app/config/packages/framework.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 24/02/2025, 18:24
+ * Last modified by "IDMarinas" on 24/02/2025, 18:28
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -28,12 +28,7 @@ return static function (ContainerConfigurator $container) {
 		'php_errors'            => [
 			'log' => true,
 		],
-		'csrf_protection'       => false,
-		'form'                  => [
-			'csrf_protection' => [
-				'enabled' => false,
-			],
-		],
+		'form'                  => true,
 		'assets'                => true,
 	]);
 };
