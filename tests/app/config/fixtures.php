e6d14b674eff2059eb8581dbf59de17a64572f9c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:14:51 +0100

    test(config): relocate factories and fixtures configuration to test directory

--- tests/app/config/fixtures.php
+++ tests/app/config/fixtures.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 16/01/2025, 21:24
+ * Last modified by "IDMarinas" on 06/01/2026, 19:14
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
  *
- * @file    factories.php
+ * @file    fixtures.php
  * @date    16/01/2025
  * @time    20:47
  *
@@ -17,13 +17,14 @@
  * @since   3.4.0
  */
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $container) {
+return static function (ContainerConfigurator $container, ContainerBuilder $builder) {
 	// @formatter:off
 	$container
 		->services()
-			->load('Factory\\', dirname(__DIR__) . '/factories')
+			->load('DataFixtures\\', $builder->getParameter('kernel.project_dir') . '/tests/DataFixtures')
 			->public()
 			->autowire()
 			->autoconfigure()
