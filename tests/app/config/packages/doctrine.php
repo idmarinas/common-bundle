7da04928242fd0567ef943e21202b120af6d3be2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:21:57 +0100

    test(config): refactor configurations for test environment, enhancing cache and DB setup

--- tests/app/config/packages/doctrine.php
+++ tests/app/config/packages/doctrine.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/01/2026, 18:50
+ * Last modified by "IDMarinas" on 06/01/2026, 19:21
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -21,12 +21,13 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Idm\Bundle\Common\IdmCommonBundle;
 use ReflectionClass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Filesystem\Filesystem;
 use function Symfony\Component\String\u;
 
-return static function (ContainerConfigurator $container) {
-	$getDatabaseCache = function (): string {
-		$dir = dirname(__DIR__, 3) . '/var/cache/database';
+return static function (ContainerConfigurator $container, ContainerBuilder $builder) {
+	$getDatabaseCache = function (string $projectDir, string $env): string {
+		$dir = $projectDir . '/var/cache/database';
 
 		$filesystem = new Filesystem();
 
@@ -34,16 +35,16 @@ return static function (ContainerConfigurator $container) {
 			$filesystem->mkdir($dir);
 		}
 
-		return $dir;
-	};
+		$dbName = (new ReflectionClass(IdmCommonBundle::class))->getShortName();
+		$dbName = u($dbName)->snake()->toString();
 
-	$dbName = (new ReflectionClass(IdmCommonBundle::class))->getShortName();
-	$dbName = u($dbName)->snake()->toString();
+		return sprintf('sqlite:///%s/%s_%s.sqlite', $dir, $dbName, $env);
+	};
 
 	$container->extension('doctrine', [
 		'dbal' => [
 			'driver' => 'pdo_sqlite',
-			'url'    => sprintf('sqlite:///%s/%s_%s.sqlite', $getDatabaseCache(), $dbName, $container->env()),
+			'url'    => $getDatabaseCache($builder->getParameter('kernel.project_dir'), $container->env()),
 		],
 		'orm'  => [
 			'auto_mapping'        => false,
@@ -58,10 +59,10 @@ return static function (ContainerConfigurator $container) {
 					'dir'       => dirname(__DIR__, 2) . '/src/Entity',
 					'prefix'    => 'App\Entity',
 				],
-				//'resolve_target_entities' => [
-				//	AbstractUser::class => User::class,
-				//],
 			],
+			//'resolve_target_entities' => [
+			//	AbstractUser::class => User::class,
+			//],
 		],
 	]);
 };
--- tests/app/config/packages/security.php
+++ tests/app/config/packages/security.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 16/01/2025, 21:21
+ * Last modified by "IDMarinas" on 06/01/2026, 19:21
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
