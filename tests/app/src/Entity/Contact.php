799e39706e89fb3ba4f09574c8f1cd7f1718563e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:29:29 +0100

    test(entity): relocate Contact entity and repository for test environment

--- tests/app/src/Entity/Contact.php
+++ tests/app/src/Entity/Contact.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 24/02/2025, 18:42
+ * Last modified by "IDMarinas" on 06/01/2026, 19:29
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
