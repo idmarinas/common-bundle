3389e6ec9fe69fc91c0b9517c39d43fa1ffb3c25
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:13:12 +0100

    chore(fixtures): update copyright years and correct file paths in fixtures and factories

--- tests/DataFixtures/ContactFixtures.php
+++ tests/DataFixtures/ContactFixtures.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 22/01/2025, 13:33
+ * Last modified by "IDMarinas" on 06/01/2026, 19:12
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
