130ff852de6d268794fd4e4a5d5be546e8e01056
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:16:04 +0100

    test(kernel): relocate and enhance `Kernel.php` for test-specific setup and configuration

---
+++ tests/app/src/Kernel.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * Copyright 2024-2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 06/01/2026, 19:15
+ *
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    Kernel.php
+ * @date    16/01/2025
+ * @time    20:47
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   3.4.0
+ */
+
+namespace App;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+final class Kernel extends BaseKernel
+{
+	use MicroKernelTrait;
+
+	private array  $extraBundles    = [];
+	private array  $extraRoutes     = [];
+	private array  $extraConfig     = [];
+	private string $testCachePrefix = '';
+	private bool   $clearCache      = true;
+
+	public function __construct (string $environment, bool $debug)
+	{
+		parent::__construct($environment, $debug);
+
+		if ('test' === $this->environment) {
+			$this->testCachePrefix = '/' . uniqid('', true);
+		}
+	}
+
+	public function addExtraBundle (string $bundleName): self
+	{
+		$this->extraBundles[$bundleName] = ['all' => true];
+
+		return $this;
+	}
+
+	public function addExtraConfig (string|array $config): self
+	{
+		if (is_array($config)) {
+			$this->extraConfig = array_merge($this->extraConfig, $config);
+		} else {
+			$this->extraConfig[] = $config;
+		}
+
+		return $this;
+	}
+
+	public function addExtraRoutesFile (string $route): self
+	{
+		$this->extraRoutes[] = $route;
+
+		return $this;
+	}
+
+	public function configureRoutes (RoutingConfigurator $routes): void
+	{
+		$extraRoutes = array_unique(array_merge([
+			$this->getConfigDir() . '/routes.php',
+			$this->getTestConfigDir() . '/routes.php',
+		], $this->extraRoutes));
+		array_walk($extraRoutes, static fn(string $route) => file_exists($route) ? $routes->import($route) : null);
+	}
+
+	public function registerBundles (): iterable
+	{
+		$contents = require $this->getBundlesPath();
+		$contents = array_merge($contents, $this->extraBundles);
+
+		foreach ($contents as $class => $envs) {
+			if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+				yield new $class();
+			}
+		}
+	}
+
+	public function getCacheDir (): string
+	{
+		return parent::getCacheDir() . $this->testCachePrefix;
+	}
+
+	public function shutdown (): void
+	{
+		parent::shutdown();
+
+		if (!$this->clearCache) {
+			return;
+		}
+
+		$cacheDirectory = $this->getCacheDir();
+		$logDirectory = $this->getLogDir();
+
+		$filesystem = new Filesystem();
+
+		if ($filesystem->exists($cacheDirectory)) {
+			$filesystem->remove($cacheDirectory);
+		}
+
+		if ($filesystem->exists($logDirectory)) {
+			$filesystem->remove($logDirectory);
+		}
+	}
+
+	public function clearCacheAfterShutdown (): self
+	{
+		$this->clearCache = true;
+
+		return $this;
+	}
+
+	public function notClearCacheAfterShutdown (): self
+	{
+		$this->clearCache = false;
+
+		return $this;
+	}
+
+	/**
+	 * For add more config/routes/bundles to kernel
+	 *
+	 * <code>
+	 *   <?php
+	 *    use Symfony\Bundle\FrameworkBundle\Test\{
+	 *      KernelTestCase,
+	 *      WebTestCase
+	 *    };
+	 *
+	 *    class TestKernel extends [(KernelTestCase|WebTestCase)]
+	 *    {
+	 *      public function testAnything (): void
+	 *      {
+	 *        $kernel = self::bootKernel([
+	 *          'config' => static function (Kernel $kernel) {
+	 *            $kernel->addExtraBundle(BundleName::class);
+	 *            $kernel->addExtraConfigFile('path/to/file.php');
+	 *            $kernel->addExtraRoutesFile('path/to/file.php');
+	 *          }
+	 *        ]);
+	 *      }
+	 *    }
+	 * </code>
+	 */
+	public function handleOptions (array $options): void
+	{
+		if (array_key_exists('config', $options) && is_callable($config = $options['config'])) {
+			$config($this);
+		}
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	protected function configureContainer (
+		ContainerConfigurator $container,
+		LoaderInterface       $loader,
+		ContainerBuilder      $builder
+	): void {
+		// Load config for Test App
+		$extensions = $builder->getExtensions();
+		$fileName = fn(string $name): string => $this->getTestPackagesConfigDir() . '/' . $name . '.php';
+		$file = fn(string $name): ?string => file_exists($fileName($name)) ? $fileName($name) : null;
+		array_walk($extensions, static fn(ExtensionInterface &$ext, string $name) => $ext = $file($name));
+
+		$config = [
+			// Load service of Bundle
+			$this->getTestConfigDir() . '/services.php',
+			// Load Fixtures and Factories of Bundle
+			$this->getTestConfigDir() . '/factories.php',
+			$this->getTestConfigDir() . '/fixtures.php',
+		];
+
+		$full = array_filter($extensions + $config);
+		$load = fn(string|int $ext): bool => is_numeric($ext) || $builder->hasExtension($ext);
+		array_walk($full, static fn(string $file, int|string $ext) => $load($ext) ? $loader->load($file) : null);
+
+		foreach ($this->extraConfig as $extension => $config) {
+			if (is_array($config)) {
+				$builder->loadFromExtension($extension, $config);
+			} else {
+				$loader->load($config);
+			}
+		}
+	}
+
+	private function getBundlesPath (): string
+	{
+		return $this->getTestConfigDir() . '/bundles.php';
+	}
+
+	private function getTestConfigDir (): string
+	{
+		return $this->getProjectDir() . '/tests/app/config';
+	}
+
+	private function getTestPackagesConfigDir (): string
+	{
+		return $this->getTestConfigDir() . '/packages';
+	}
+}
