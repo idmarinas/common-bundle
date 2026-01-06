6b6263b5338eb7d592e44f963e220e0e108a7896
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:33:40 +0100

    test(config): relocate and enhance routes configuration for test environment

--- tests/app/config/routes.php
+++ tests/app/config/routes.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 27/01/2025, 20:51
+ * Last modified by "IDMarinas" on 06/01/2026, 19:33
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -19,17 +19,30 @@
 
 use App\Controller\Admin\DashboardController;
 use App\Controller\ContactController;
+use Symfony\Bundle\FrameworkBundle\Controller\TemplateController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
 	// @formatter:off
+//	$routes->import('routes/web_profiler.php');
+
+	//$routes->import('security.route_loader.logout', 'service')->methods(['GET']);
+
 	$routes
 		->import(ContactController::class, 'attribute')
-			->namePrefix('idm_common_')
+		->namePrefix('idm_common_')
 	;
 	$routes
 		->import(DashboardController::class, 'attribute')
-			->namePrefix('idm_admin_')
+		->namePrefix('idm_admin_')
+	;
+	$routes->add('app_home', '/')
+		->controller(TemplateController::class)
+		->methods(['GET'])
+		->defaults(['template' => 'pages/home.html.twig'])
+		->options([
+			'seo' => true
+		])
 	;
 	// @formatter:on
 };
