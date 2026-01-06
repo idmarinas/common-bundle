cf1233e66bbd90b01ed05ab7c091c19a10f34342
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:17:44 +0100

    test(routing): add web profiler routes configuration for tests

---
+++ tests/app/config/routes/web_profiler.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 04/01/2026, 20:49
+ *
+ * @project IDMarinas Common Bundle
+ * @see     https://github.com/idmarinas/common-bundle
+ *
+ * @file    web_profiler.php
+ * @date    07/11/2025
+ * @time    14:56
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) {
+	// @formatter:off
+	$routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml')->prefix('/_wdt');
+	$routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')->prefix('/_profiler');
+
+
+	// @formatter:on
+};
