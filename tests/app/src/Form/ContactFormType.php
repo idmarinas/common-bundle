c576b29fcc80f330d575078cf39c44243dd0facd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:30:02 +0100

    test(enum, form): relocate TestEnum and ContactFormType for test environment

--- tests/app/src/Form/ContactFormType.php
+++ tests/app/src/Form/ContactFormType.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 22/01/2025, 19:37
+ * Last modified by "IDMarinas" on 06/01/2026, 19:29
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
