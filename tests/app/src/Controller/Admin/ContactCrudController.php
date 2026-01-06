a4ae523fc3f814fcf022d8837b0ca258df4f8a65
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:30:50 +0100

    test(controller): relocate admin and contact controllers for test environment

--- tests/app/src/Controller/Admin/ContactCrudController.php
+++ tests/app/src/Controller/Admin/ContactCrudController.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 27/01/2025, 19:45
+ * Last modified by "IDMarinas" on 06/01/2026, 19:30
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
--- tests/app/src/Controller/ContactController.php
+++ tests/app/src/Controller/ContactController.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 23/01/2025, 16:12
+ * Last modified by "IDMarinas" on 06/01/2026, 19:30
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
