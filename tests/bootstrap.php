94fc38ace721553eadd0f6881d917a85e6ed9756
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-06 19:12:08 +0100

    test(bootstrap): remove unused kernel and filesystem setup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 16/01/2025, 21:17
+ * Last modified by "IDMarinas" on 06/01/2026, 19:12
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -17,18 +17,4 @@
  * @since   3.4.0
  */
 
-use App\Kernel;
-use Symfony\Component\Filesystem\Filesystem;
-
 require dirname(__DIR__) . '/vendor/autoload.php';
-
-$kernel = new Kernel('test', true);
-$filesystem = new Filesystem();
-
-if ($filesystem->exists($kernel->getCacheDir())) {
-	$filesystem->remove($kernel->getCacheDir());
-}
-
-if ($filesystem->exists($kernel->getLogDir())) {
-	$filesystem->remove($kernel->getLogDir());
-}
