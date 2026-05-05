c2ca02abceb04eead8d1b4fbd8965294bbef2f22
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-05 15:42:50 +0200

    test: remove redundant header comments in test classes

--- tests/BundleInitializationTest.php
+++ tests/BundleInitializationTest.php
@@ -1,28 +1,9 @@
 <?php
+
 /**
  * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 05/05/2026, 15:20
- *
- * @project IDMarinas Common Bundle
- * @see     https://github.com/idmarinas/common-bundle
- *
- * @file    BundleInitializationTest.php
- * @date    05/05/2026
- * @time    15:26
- *
- * @author  Iván Diaz Marinas (IDMarinas)
- * @license BSD 3-Clause License
- *
- * @since   3.7.0
- */
-
-declare(strict_types=1);
-
-/**
- * Copyright 2025-2026 (C) IDMarinas - All Rights Reserved
- *
- * Last modified by "IDMarinas" on 06/01/2026, 19:11
+ * Last modified by "IDMarinas" on 05/05/2026, 15:42
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -36,6 +17,8 @@ declare(strict_types=1);
  *
  * @since   3.4.0
  */
+
+declare(strict_types=1);
 
 namespace Idm\Bundle\Common\Tests;
 
