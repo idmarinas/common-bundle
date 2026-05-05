fdcd614979b923fa263c7a9f57e4d0965cec2532
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-05 10:26:14 +0200

    feat(bundle): add `configure` method to support definition imports

--- src/IdmCommonBundle.php
+++ src/IdmCommonBundle.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright 2022-2025 (C) IDMarinas - All Rights Reserved
+ * Copyright $originalComment.match("Copyright (\d+)", 1, "-",$today.year)2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 19/01/2025, 19:32
+ * Last modified by "IDMarinas" on 04/05/2026, 20:55
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -19,14 +19,23 @@
 
 namespace Idm\Bundle\Common;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class IdmCommonBundle extends AbstractBundle
 {
-	public function loadExtension (array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+	public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
-		$container->import(dirname(__DIR__) . '/config/services.php');
+		$container->import(dirname(__DIR__).'/config/services.php');
 	}
+
+	public function configure(DefinitionConfigurator $definition): void
+	{
+		$definition->import(dirname(__DIR__).'/config/definitions.php');
+
+		parent::configure($definition);
+	}
+
 }
