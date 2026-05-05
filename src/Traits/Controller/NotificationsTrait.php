89b4c49363defe9ad48f2dce6dcb9d92d6ed0448
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-05 18:49:47 +0200

    refactor(NotificationsTrait): simplify session handling logic

--- src/Traits/Controller/NotificationsTrait.php
+++ src/Traits/Controller/NotificationsTrait.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright $originalComment.match("Copyright (\d+)", 1, "-",$today.year)2026 (C) IDMarinas - All Rights Reserved
+ * Copyright 2026 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 04/05/2026, 20:54
+ * Last modified by "IDMarinas" on 05/05/2026, 18:48
  *
  * @project IDMarinas Common Bundle
  * @see     https://github.com/idmarinas/common-bundle
@@ -20,11 +20,7 @@
 namespace Idm\Bundle\Common\Traits\Controller;
 
 use Idm\Bundle\Common\Bag\NotificationsBag;
-use LogicException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
-use Symfony\Component\HttpFoundation\Session\FlashBagAwareSessionInterface;
-use function sprintf;
 
 /**
  * @property ContainerInterface $container
@@ -34,24 +30,7 @@ trait NotificationsTrait
 
 	protected function addNotification(string $type, mixed $message): void
 	{
-		try {
-			$session = $this->container->get('request_stack')->getSession();
-		} catch (SessionNotFoundException $e) {
-			throw new LogicException(
-				'You cannot use the addFlash method if sessions are disabled. Enable them in "config/packages/framework.yaml".',
-				0, $e
-			);
-		}
-
-		if (!$session instanceof FlashBagAwareSessionInterface) {
-			throw new LogicException(
-				sprintf(
-					'You cannot use the addFlash method because class "%s" doesn\'t implement "%s".',
-					get_debug_type($session),
-					FlashBagAwareSessionInterface::class
-				)
-			);
-		}
+		$session = $this->container->get('request_stack')->getSession();
 
 		/** @var NotificationsBag $notification */
 		$notification = $session->getBag('notifications');
