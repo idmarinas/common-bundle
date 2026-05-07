f885f11400ba63f30113a5e4a6efab6f73d1a2c2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-07 17:56:42 +0200

    docs(config): extend `docsBundle` with libraries, socials, and support links

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,46 @@
 export default defineNuxtConfig({
 	extends: ['github:idmarinas/nuxt-layers/docs-bundle#master', 'docus'],
-	docsBundle: {},
+	docsBundle: {
+		libraries: [
+			{
+				title: 'Symfony Components',
+				icon: 'i-tabler-brand-symfony',
+				to: 'https://www.symfony.com/components',
+				description: 'This project relies on these components for most of its features.'
+			},
+			{
+				title: 'Other component',
+				icon: 'i-tabler-components',
+				to: 'https://www.example.com',
+				description: 'Other Component used in this project.'
+			}
+		],
+		socials: {
+			x: 'https://x.com/idmarinas',
+			reddit: 'https://reddit.com/u/idmarinas',
+			paypal: 'https://www.paypal.me/idmarinas',
+			bitly: 'https://bit.ly/m/idmarinas',
+			githubsponsors: 'https://github.com/sponsors/idmarinas',
+			linkedin: 'https://linkedin.com/in/idmarinas',
+		},
+		support_links: {
+			title: 'Support me',
+			links: [
+				{
+					icon: 'i-tabler-brand-paypal',
+					label: 'PayPal.Me',
+					to: 'https://www.paypal.me/idmarinas',
+					target: '_blank'
+				},
+				{
+					icon: 'i-tabler-brand-github',
+					label: 'GitHub Sponsor',
+					to: 'https://github.com/sponsors/idmarinas',
+					target: '_blank'
+				}
+			]
+		},
+	},
 	$production: {
 		llms: {
 			domain: 'https://idmarinas.github.io/common-bundle'
